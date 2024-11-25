7c93fec8efa82e659347c5ef8661a60c40370c56
Jquest64 <mjquest64@outlook.com>
2024-11-25 18:19:41 -0300

    Initial commit

---
+++ src/main/java/dio/desafio_projeto_dev_2024/DesafioProjetoDev2024Application.java
@@ -0,0 +1,13 @@
+package dio.desafio_projeto_dev_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioProjetoDev2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioProjetoDev2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/dio/desafio_projeto_dev_2024/DesafioProjetoDev2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.desafio_projeto_dev_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioProjetoDev2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
